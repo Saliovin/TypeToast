e0b5c27dc2b0f8eb7c25fc61ced5c3fa5b453f74
Saliovin <jonas.eukan@gmail.com>
2022-06-22 19:36:14 +0800

    Simplified timeout logic

--- hooks/useTimer.tsx
+++ hooks/useTimer.tsx
@@ -3,11 +3,11 @@ import { useEffect, useRef, useState } from "react";
 type Callback = () => void;
 
 const useTimer = (
-  intervalCallback?: Callback,
-  timeoutCallback?: Callback
+  timeoutCallback: Callback,
+  intervalCallback?: Callback
 ): [number, (time: number) => void] => {
   const [time, setTime] = useState(0);
-  const [timeLeft, setTimeLeft] = useState(0);
+  const [timeLeft, setTimeLeft] = useState(-1);
   const intervalFunc = useRef(() => {
     return;
   });
@@ -22,25 +22,23 @@ const useTimer = (
       setTimeLeft(timeLeft - 1);
       if (intervalCallback) intervalCallback();
     };
-  }, [timeLeft, intervalCallback]);
+    if (timeLeft === 0) {
+      setTime(0);
+      setTimeLeft(-1);
+      if (timeoutCallback) timeoutCallback();
+    }
+  }, [timeLeft, intervalCallback, timeoutCallback]);
 
   useEffect(() => {
     if (time === 0) return;
-
     const intervalId = window.setInterval(() => {
       intervalFunc.current();
     }, 1000);
-    const timeoutId = window.setTimeout(() => {
-      intervalFunc.current();
-      if (timeoutCallback) timeoutCallback();
-      setTime(0);
-    }, time * 1000);
 
     return () => {
       clearInterval(intervalId);
-      clearTimeout(timeoutId);
     };
-  }, [time, timeoutCallback]);
+  }, [time]);
 
   return [timeLeft, setTimer];
 };
