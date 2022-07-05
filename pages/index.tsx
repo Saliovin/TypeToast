e4e00c85ce8ff64eddb38fb4a2b66b547b123805
Saliovin <jonas.eukan@gmail.com>
2022-07-05 18:21:55 +0800

    Improved performance and added Timer component

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,86 +12,69 @@ import React, {
 import Caret from "../components/Caret";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import Timer from "../components/Timer";
 import WordSet from "../components/WordSet";
+import useTimer from "../hooks/useTimer";
 import styles from "../styles/Home.module.css";
+import wordList from "../wordlist.json";
 
 const Home: NextPage = () => {
-  const testList = [
-    "hello",
-    "come",
-    "ask",
-    "their",
-    "upcoming",
-    "soft",
-    "tell",
-    "borrow",
-    "feet",
-    "house",
-    "drop",
-    "most",
-    "leap",
-    "test",
-    "pool",
-    "from",
-    "help",
-    "where",
-    "make",
-    "test",
-    "all",
-    "comfort",
-    "true",
-    "accent",
-    "tore",
-    "pines",
-    "test",
-    "gold",
-    "west",
-    "repeat",
-    "barret",
-    "normal",
-    "cast",
-    "less",
-    "peace",
-    "quarts",
-  ];
-  const [typedWordList, setTypedWordList] = useState("");
+  const [typedWordList, setTypedWordList] = useState<string[]>([""]);
   const [activeWordIndex, setActiveWordIndex] = useState(0);
-  const [activeLetterIndex, setActiveLetterIndex] = useState(0);
   const [wordRect, setWordRect] = useState({ top: 0, left: 0 });
+  const [testStart, setTestStart] = useState(false);
+  const [wordSet, setWordSet] = useState<string[]>([]);
+  const [timeLeft, startTimer] = useTimer(() => {
+    let correctCharNum = 0;
+    setTestStart(false);
+    typedWordList.forEach((typedWord, i) => {
+      typedWord.split("").forEach((typedChar, j) => {
+        if (wordSet[i].charAt(j) === typedChar) correctCharNum++;
+      });
+    });
+    correctCharNum += typedWordList.length - 1;
+    const GWPM = typedWordList.length / 5 / 0.25;
+    const NWPM = correctCharNum / 5 / 0.25;
+  });
   const wordRef = useCallback((node: HTMLDivElement) => {
     if (node === null) return;
     node.scrollIntoView({
       block: "center",
     });
+
     const rect = node.getBoundingClientRect();
     setWordRect({ top: rect?.top || 0, left: rect?.left || 0 });
   }, []);
   const main = useRef<HTMLDivElement>(null);
-
-  const handleKeyPress = ({ key }: React.KeyboardEvent) => {
-    if (key === "Backspace") {
-      setTypedWordList(typedWordList.slice(0, -1));
+  const handleKeyPress = (event: React.KeyboardEvent) => {
+    if (
+      (event.key.length !== 1 && event.key !== "Backspace") ||
+      event.shiftKey ||
+      event.altKey ||
+      event.ctrlKey
+    )
+      return;
+    if (!testStart) {
+      startTimer(15);
+      setTestStart(true);
     }
-    if (key.length !== 1) return;
+    let typed = typedWordList.join(" ");
 
-    setTypedWordList(typedWordList + key);
+    if (event.key === "Backspace") typed = typed.slice(0, -1);
+    else typed += event.key;
+    setTypedWordList(typed.split(" "));
   };
 
   useEffect(() => {
-    const typedWordListArray = typedWordList.split(" ");
-
-    setActiveWordIndex(typedWordListArray.length - 1);
-    setActiveLetterIndex((typedWordListArray.at(-1) || "").length);
+    setActiveWordIndex(typedWordList.length - 1);
   }, [typedWordList]);
-  useEffect(() => main.current?.focus());
+  useEffect(() => {
+    main.current?.focus();
+    setWordSet(wordList.sort(() => Math.random() - 0.5));
+  }, []);
 
   return (
-    <div
-      className={styles.container}
-      onKeyDown={handleKeyPress}
-      tabIndex={0}
-      ref={main}
-    >
+    <div className={styles.container}>
       <Head>
         <title>TypeToast</title>
         <meta
@@ -102,21 +85,28 @@ const Home: NextPage = () => {
       </Head>
 
       <Header />
-      {wordRect.top != 0 && (
-        <Caret
-          top={wordRect.top}
-          left={wordRect.left}
-          offset={18.37 * activeLetterIndex}
-        />
-      )}
+      <div
+        ref={main}
+        onKeyDown={handleKeyPress}
+        tabIndex={0}
+        className={styles.test}
+      >
+        <Timer timeLeft={timeLeft} />
 
-      <WordSet
-        wordList={testList}
-        typedWordList={typedWordList.split(" ")}
-        activeLetterIndex={activeLetterIndex}
-        activeWordIndex={activeWordIndex}
-        wordRef={wordRef}
-      />
+        {wordRect.top != 0 && (
+          <Caret
+            top={wordRect.top}
+            left={wordRect.left}
+            offset={18.37 * typedWordList[activeWordIndex]?.length || 0}
+          />
+        )}
+        <WordSet
+          wordList={wordSet.slice(0, activeWordIndex + 50)}
+          typedWordList={typedWordList}
+          activeWordIndex={activeWordIndex}
+          wordRef={wordRef}
+        />
+      </div>
       <Footer />
     </div>
   );
