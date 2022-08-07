c1f0dfefcb243387486ae2c10051d9879a3ff653
Saliovin <jonas.eukan@gmail.com>
2022-08-07 17:48:31 +0800

    Changed to contain links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,11 @@
 import styles from "../styles/Footer.module.css";
 
 const Footer = () => {
-  return <footer className={styles.footer}>Built with NextJS</footer>;
+  return (
+    <footer className={styles.footer}>
+      <a href="https://github.com/Saliovin/TypeToast">{"</>"}GitHub</a>
+    </footer>
+  );
 };
 
 export default Footer;
