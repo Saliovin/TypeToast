90bc4bfb53120804e16b523733b56c19c28efa1f
Saliovin <jonas.eukan@gmail.com>
2022-08-19 20:02:28 +0800

    Added favicon icons

--- pages/index.tsx
+++ pages/index.tsx
@@ -134,7 +134,24 @@ const Home: NextPage = () => {
           name="description"
           content="Simple, subtle, straightforward. TypeToast is a typing test that gives you your typing speed as easily as making toast!"
         />
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
         <meta
           name="google-site-verification"
           content="XCTjZzY_AZ8tEPP-AqJQ_RBQhjGmmGqf0W-nAeZ3r0Y"
