70fc7bd2749986478be1a9ca89e08b2610ea6e44
Saliovin <jonas.eukan@gmail.com>
2023-01-01 19:33:49 +0800

    Added google analytics tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,27 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html>
       <Head>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-3CFFDZN9NN"
+        ></Script>
+        <Script
+          id="google-analytics"
+          dangerouslySetInnerHTML={{
+            __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+
+                    gtag('config', 'G-3CFFDZN9NN');
+                    `,
+          }}
+        />
+
         <link
           href="https://fonts.googleapis.com/css2?family=Inter&display=optional"
           rel="stylesheet"
