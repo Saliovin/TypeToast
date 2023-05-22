e5deb4253f0f0e3280da4c3573428832df3bd100
Saliovin <jonas.eukan@gmail.com>
2023-05-22 18:00:43 +0800

    Updated to be ARIA compliant and added description to leaderboard

--- components/Leaderboard.tsx
+++ components/Leaderboard.tsx
@@ -14,42 +14,7 @@ const Leaderboard = ({ records }: Props) => {
   return (
     <Dialog.Root>
       <Dialog.Trigger asChild>
-        <button className={styles.leaderboardButton}>
-          <svg
-            width="30"
-            height="30"
-            strokeWidth="1.2"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            {" "}
-            <path
-              d="M15 21H9V12.6C9 12.2686 9.26863 12 9.6 12H14.4C14.7314 12 15 12.2686 15 12.6V21Z"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />{" "}
-            <path
-              d="M20.4 21H15V18.1C15 17.7686 15.2686 17.5 15.6 17.5H20.4C20.7314 17.5 21 17.7686 21 18.1V20.4C21 20.7314 20.7314 21 20.4 21Z"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />{" "}
-            <path
-              d="M9 21V16.1C9 15.7686 8.73137 15.5 8.4 15.5H3.6C3.26863 15.5 3 15.7686 3 16.1V20.4C3 20.7314 3.26863 21 3.6 21H9Z"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />{" "}
-            <path
-              d="M10.8056 5.11325L11.7147 3.1856C11.8314 2.93813 12.1686 2.93813 12.2853 3.1856L13.1944 5.11325L15.2275 5.42427C15.4884 5.46418 15.5923 5.79977 15.4035 5.99229L13.9326 7.4917L14.2797 9.60999C14.3243 9.88202 14.0515 10.0895 13.8181 9.96099L12 8.96031L10.1819 9.96099C9.94851 10.0895 9.67568 9.88202 9.72026 9.60999L10.0674 7.4917L8.59651 5.99229C8.40766 5.79977 8.51163 5.46418 8.77248 5.42427L10.8056 5.11325Z"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />{" "}
-          </svg>
-        </button>
+        <button className={styles.leaderboardButton}>Leaderboard</button>
       </Dialog.Trigger>
       <Dialog.Portal>
         <Dialog.Overlay className={styles.DialogOverlay} />
@@ -57,24 +22,26 @@ const Leaderboard = ({ records }: Props) => {
           <Dialog.Title className={styles.DialogTitle}>
             Leaderboard
           </Dialog.Title>
-          <Dialog.Description />
+          <Dialog.Description>Weekly</Dialog.Description>
           <table className={styles.leaderboard}>
-            <tr>
-              <th>Rank</th>
-              <th>Name</th>
-              <th>WPM</th>
-            </tr>
-            {records.map((record, index) => {
-              console.log(record);
-
-              return (
-                <tr key={record.timestamp.toMillis()}>
-                  <td>{index + 1}</td>
-                  <td>{record.name}</td>
-                  <td>{record.wpm}</td>
-                </tr>
-              );
-            })}
+            <thead>
+              <tr>
+                <th>Rank</th>
+                <th>Name</th>
+                <th>WPM</th>
+              </tr>
+            </thead>
+            <tbody>
+              {records.map((record, index) => {
+                return (
+                  <tr key={record.timestamp.toMillis()}>
+                    <td>{index + 1}</td>
+                    <td>{record.name}</td>
+                    <td>{record.wpm}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
           </table>
           <Dialog.Close asChild>
             <button className={styles.IconButton} aria-label="Close">
